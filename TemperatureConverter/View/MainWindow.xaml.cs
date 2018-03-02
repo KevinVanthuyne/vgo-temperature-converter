3da8bbb47d674d67e2933b673c3e2b6eb9b89ac9
KevinVanthuyne <kevin.vanthuyne2@gmail.com>
2018-03-02 12:06:45 +0100

    added Kelvin conversion

--- TemperatureConverter/View/MainWindow.xaml.cs
+++ TemperatureConverter/View/MainWindow.xaml.cs
@@ -25,23 +25,41 @@ namespace View
             InitializeComponent();
         }
 
-        private void ConvertToCelsius(object sender, RoutedEventArgs e)
+        private void ConvertFahrenheit(object sender, RoutedEventArgs e)
         {   
             // read from Fahrenheit textbox
             String str = FahrenheitTextBox.Text;
-            var degrees = double.Parse(str);
-            // convert Fahrenheit to Celsius
-            degrees = Math.Round((degrees - 32) / 1.8, 2);
-            // display in celsius textbox
-            CelsiusTextBox.Text = degrees.ToString();
+            var fahrenheit = double.Parse(str);
+            // convert Fahrenheit
+            var celsius = Math.Round((fahrenheit - 32) / 1.8, 2);
+            var kelvin = Math.Round((fahrenheit + 459.67) * 5 / 9);
+            // display in conversions
+            CelsiusTextBox.Text = celsius.ToString();
+            KelvinTextBox.Text = kelvin.ToString();
         }
 
-        private void ConvertToFahrenheit(object sender, RoutedEventArgs e)
+        private void ConvertCelsius(object sender, RoutedEventArgs e)
         {
             String str = CelsiusTextBox.Text;
-            var degrees = double.Parse(str);
-            degrees = Math.Round(degrees * 1.8 + 32, 2);
-            FahrenheitTextBox.Text = degrees.ToString();
+            var celsius = double.Parse(str);
+
+            var fahrenheit = Math.Round(celsius * 1.8 + 32, 2);
+            var kelvin = Math.Round(celsius + 273.15, 2);
+
+            FahrenheitTextBox.Text = fahrenheit.ToString();
+            KelvinTextBox.Text = kelvin.ToString();
+        }
+
+        private void ConvertKelvin(object sender, RoutedEventArgs e)
+        {
+            String str = KelvinTextBox.Text;
+            var kelvin = double.Parse(str);
+
+            var celsius = Math.Round(kelvin - 273.15, 2);
+            var fahrenheit = Math.Round(kelvin * 9 / 5 - 459.67, 2);
+
+            FahrenheitTextBox.Text = fahrenheit.ToString();
+            CelsiusTextBox.Text = celsius.ToString();
         }
     }
 }
