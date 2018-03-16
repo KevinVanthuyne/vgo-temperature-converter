463d640c53026d9a287d9bcd4abd80e66e0dc43d
KevinVanthuyne <kevin.vanthuyne2@gmail.com>
2018-03-16 11:59:32 +0100

    celsius convertor

--- TemperatureConverter/View/MainWindow.xaml.cs
+++ TemperatureConverter/View/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -67,6 +68,25 @@ namespace View
             //KelvinTextBox.Text = kelvin.ToString();
 
             //ConvertKelvin(kelvin);
+        }
+    }
+
+    public class CelsiusConvertor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var kelvin = (double)value;
+            var celsius = kelvin - 273.15;
+
+            return celsius.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var celsius = double.Parse((string)value);
+            var kelvin = celsius + 273.15;
+
+            return kelvin;
         }
     }
 }
