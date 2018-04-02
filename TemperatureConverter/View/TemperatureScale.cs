0a423649a90b0cc3b137c55a0ceb0bfd43317ade
KevinVanthuyne <kevin.vanthuyne2@gmail.com>
2018-04-02 14:25:10 +0200

    Fahrenheit & Celsius TemperatureScales

--- TemperatureConverter/View/TemperatureScale.cs
+++ TemperatureConverter/View/TemperatureScale.cs
@@ -4,21 +4,50 @@ public interface ITemperatureScale
     string Name { get; }
 
     double ConvertToKevin(double temperature);
-    double ConvertFromKelvin(double temperature);
+    double ConvertFromKelvin(double kelvin);
 }
 
 public class KelvinTemperatureScale : ITemperatureScale
 {
     public string Name => "Kelvin";
 
-    public double ConvertFromKelvin(double temperature)
+    public double ConvertFromKelvin(double kelvin)
     {
-        return temperature;
+        return kelvin;
     }
 
-    public double ConvertToKevin(double temperature)
+    public double ConvertToKevin(double kelvin)
     {
-        return temperature;
+        return kelvin;
     }
 }
 
+public class CelsiusTemperatureScale : ITemperatureScale
+{
+    public string Name => "Celsius";
+
+    public double ConvertFromKelvin(double kelvin)
+    {
+        return kelvin - 273.15;
+    }
+
+    public double ConvertToKevin(double celsius)
+    {
+        return celsius + 273.15;
+    }
+}
+
+public class FahrenheitTemperatureScale : ITemperatureScale
+{
+    public string Name => "Fahrenheit";
+
+    public double ConvertFromKelvin(double kelvin)
+    {
+        return kelvin * 9 / 5 - 459.67;
+    }
+
+    public double ConvertToKevin(double fahrenheit)
+    {
+        return (fahrenheit + 459.67) * 5 / 9;
+    }
+}
