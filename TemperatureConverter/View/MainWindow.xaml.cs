13c641a5468df8ef4048954ac9d755a3a1660c94
KevinVanthuyne <kevin.vanthuyne2@gmail.com>
2018-04-05 15:38:03 +0200

    removed TemperatureConverter class

--- TemperatureConverter/View/MainWindow.xaml.cs
+++ TemperatureConverter/View/MainWindow.xaml.cs
@@ -28,21 +28,5 @@ namespace View
         }
     }
 
-    public class TemperatureConvertor : IValueConverter
-    {
-        public ITemperatureScale TemperatureScale { get; set; }
-
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            var kelvin = (double)value;
-            return TemperatureScale.ConvertFromKelvin(kelvin).ToString();
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            var temp = (double)value;
-            return TemperatureScale.ConvertToKevin(temp).ToString();
-        }
-    }
 }
 
